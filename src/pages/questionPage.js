2a79b2883e76dcb3b29ba15a1a5e91649137c109
akin tanis <akin.tanis.dev@gmail.com>
2022-06-19 15:27:40 +0200

    feature: options are got selectable by users

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -2,6 +2,8 @@
 
 import {
   ANSWERS_LIST_ID,
+  ANSWERS_OPTION_ID,
+  ANSWERS_OPTION_RADIO_BUTTON_ID,
   NEXT_QUESTION_BUTTON_ID,
   USER_INTERFACE_ID,
 } from '../constants.js';
@@ -13,7 +15,7 @@ export const initQuestionPage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
 
-  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  const currentQuestion = getCurrentQuestion();
 
   const questionElement = createQuestionElement(currentQuestion.text);
 
@@ -22,8 +24,16 @@ export const initQuestionPage = () => {
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
-    const answerElement = createAnswerElement(key, answerText);
+    const answerElement = createAnswerElement(
+      key,
+      answerText,
+      currentQuestion.selected
+    );
     answersListElement.appendChild(answerElement);
+
+    document
+      .getElementById(ANSWERS_OPTION_RADIO_BUTTON_ID + '_' + key)
+      .addEventListener('change', changeOption.bind(null, key));
   }
 
   document
@@ -35,4 +45,28 @@ const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
   initQuestionPage();
+};
+
+const changeOption = (key) => {
+  getCurrentQuestion().selected = key;
+  clearAllSelections();
+  selectAnswer(key);
+};
+
+const clearAllSelections = () => {
+  Array.from(document.getElementById(ANSWERS_LIST_ID).children).forEach(
+    (li) => {
+      li.classList.remove('selected-answer');
+    }
+  );
+};
+
+const selectAnswer = (key) => {
+  document
+    .getElementById(ANSWERS_OPTION_ID + '_' + key)
+    .classList.add('selected-answer');
+};
+
+const getCurrentQuestion = () => {
+  return quizData.questions[quizData.currentQuestionIndex];
 };
