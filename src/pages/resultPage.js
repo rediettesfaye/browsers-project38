8216ae8992d8d16907ecb028ce21b09a3f0a0b10
Ashraf-Alshashaa <ashraf.alchechae@gmail.com>
2022-06-25 19:02:34 +0200

    add rest button to result paje

--- src/pages/resultPage.js
+++ src/pages/resultPage.js
@@ -1,11 +1,24 @@
 import { USER_INTERFACE_ID } from '../constants.js';
+import { RESULT_BUTTON_ID } from '../constants.js';
 import { pageTransitionService } from '../services/pageTransitionService.js';
 import showResult from '../views/resultView.js';
+import { 
+  createButton, 
+  resetQuiz
+} from '../pages/questionPage.js';
+
 
 export const resultPage = () => {
   const container = pageTransitionService.getIdleContainer();
   const result = showResult();
   container.appendChild(result);
+  container.appendChild(
+    createButton({
+      id: RESULT_BUTTON_ID,
+      text: 'RESTART QUIZ',
+      callback: resetQuiz,
+    })
+  );
 
   pageTransitionService.slide();
 };
